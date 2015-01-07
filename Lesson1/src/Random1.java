3df2b12d25b3cf92ebd206a517205f4d3de7658b
john <jrr@panix.com>
2015-01-07 08:31:03 -0500

    added sequential

--- Lesson1/src/Random1.java
+++ Lesson1/src/Random1.java
@@ -1,15 +1,42 @@
-import java.util.Random;
+// Course      : CMP-129
+// Chapter     : Lesson1 
+// Title       : Random Number Generator     
+// Instructor  : JReynolds
 
+// Course      : CMP-129
+// Chapter     : Lesson1 
+// Title       : Generate Arrays of Random numbers
+// Instructor  : JReynolds
+
+
+import java.util.Random;
 
 class Random1 {
 
-    // Create a Random Array of Integers
-    public static int [] createInt( int Size , int Range ) {
+    //
+    // Create a Array of Random Integers 
+    // Inputs:
+    //    int Size - the number of element in the array
+    //    int Range - randome number range [0 , Range-1]
+    // Output :
+    //    int [] - Randomly generated int Array of length=Size
+    //
+    public static int [] RandomIntArray( int Size , int Range ) {
 	if ( Size <= 0 ) return new int[0];
 	int [] A = new int[Size];
 	Random gen = new Random();
 	for(int i = 0; i < A.length ; i++)
 	    A[i] = gen.nextInt(Range);
+	return A;
+    }
+
+    //
+    // Create a Array of Sequential Integers (NonRandom)
+    public static int [] SequentialIntArray( int Size , int Start , int Step ) {
+	if ( Size <= 0 ) return new int[0];
+	int [] A = new int[Size];
+	for(int i = 0,v=Start; i < A.length ; i++,v+=Step )
+	    A[i] = v;
 	return A;
     }
 
