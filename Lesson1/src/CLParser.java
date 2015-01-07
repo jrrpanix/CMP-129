12c40233d16dc90df1144f70746e6ffabd5964b7
john <jrr@panix.com>
2015-01-07 07:01:15 -0500

    added comments

--- Lesson1/src/CLParser.java
+++ Lesson1/src/CLParser.java
@@ -1,5 +1,10 @@
 public class CLParser {
 
+    //
+    // This code parses command line input of the form
+    // --tag1=value1 --tag2=value2 ... 
+    //
+
     private String ErrorMessage_;
     
     public String getErrorMessage() {
@@ -7,16 +12,20 @@ public class CLParser {
     }
     
     public String[] parseToken( String s) {
+	// break the token at the delimiter which in this case is '='
 	String[] token = s.split("=");
 	return token;
     }
     
     public String extractKey( String s ) {
+	// the input string is of the form --<somekey>
+	// this code strips off the '--'
 	if (s.length() < 2) return new String();
 	return s.substring(2);
     }
     
     public String getCMDLine( String []args) {
+	// reform the command line from the input array into a single string
 	String cmd = new String();
 	for( String s : args )
 	    cmd += s + " " ;
@@ -25,6 +34,8 @@ public class CLParser {
     
     public boolean parse(String[] args ) {
 	boolean rv = true;
+	// iterate across all input arguments and get the key and value
+	// and print them out
 	for( String s : args ) {
 	    String[] token = parseToken(s);
 	    if ( token.length != 2 ) {
@@ -45,7 +56,14 @@ public class CLParser {
     }
     
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	// Sample command line 
+	// java CLParser --name=john --college=ccm
+	// 
+	// output
+	//
+	// Key=name,Value=john
+	// Key=college,Value=ccm
+
 	CLParser parser = new CLParser();
 	System.out.println("Starting CLParser-1 with command line :" + parser.getCMDLine(args));
 	boolean rv = parser.parse(args);
