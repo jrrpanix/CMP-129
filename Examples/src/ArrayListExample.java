5c5570e93977bc307f0a7945ec1d4254b858bfae
john <jrr@panix.com>
2015-01-12 07:51:16 -0500

    arraylist

---
+++ Examples/src/ArrayListExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class ArrayListExample {
+
+    public static void iterateList( ArrayList<String> A ) {
+	for( Iterator<String> it = A.iterator() ; it.hasNext() ; ) {
+	    System.out.println( it.next() );
+	}
+    }
+
+    public static ArrayList<String> copyArgs( String [] args ) {
+	ArrayList<String> A = new ArrayList<String>();
+	for( String a : args )
+	    A.add(a);
+	return A;
+    }
+
+    public static void main( String [] args ) {
+	ArrayList<String> A = copyArgs(args);
+	iterateList( A);
+    }
+
+}
