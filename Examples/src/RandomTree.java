7a392e23462a07c90b89ffcb62cf8c6ad5e65664
JOHN REYNOLDS <jrr@panix.com>
2015-01-19 12:14:02 -0500

    add tree builder

--- Examples/src/RandomTree.java
+++ Examples/src/RandomTree.java
@@ -6,20 +6,36 @@ class RandomTree {
     private int       height_ ;
     private int [] [] tree_   ;
 
-    RandomTree( int height ) {
+    public RandomTree( int height ) {
 	height_=height;
 	buildTree();
     }
 
-    void buildTree() {
+    private void buildTree() {
 	tree_ = new int[height_][];
-	
+	for( int i = 0; i < height_ ;i++ )
+	    tree_[i] = randomArray(i+1);
     }
 
-    int [] randomArray( int size ) {
+    private int [] randomArray( int size ) {
 	int [] A = new int[size];
-	//Random rand = new Random();
+	Random rand = new Random();
+	for( int i = 0; i < size ; i++ )
+	    A[i]=rand.nextInt(99);
 	return A;
+    }
+
+    public void print() {
+	for( int i = 0; i < tree_.length ; i++  ) {
+	    for( int j = 0 ; j < tree_[i].length ; j++ )
+		System.out.print( tree_[i][j] + " " );
+	    System.out.println("");
+	}
+    }
+
+    static public void main( String [] args ) {
+	RandomTree tree = new RandomTree(25);
+	tree.print();
     }
 
 }
