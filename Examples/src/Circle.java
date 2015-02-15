75f0f27a5551e2297a825ab340d4788cf89870d0
john <jrr@panix.com>
2015-02-15 13:03:46 -0500

    Shape

---
+++ Examples/src/Circle.java
@@ -0,0 +1,36 @@
+public class Circle extends Shape {
+    private double radius_;
+
+    public Circle() {
+	radius_=0;
+    }
+
+    public Circle( long id , double r ) {
+	super(id);
+	radius_=r;
+    }
+
+    public Circle( Circle c ) {
+	super(c);
+	radius_=c.radius_;
+    }
+
+    public double area() {
+	return Math.PI * radius_ * radius_;
+    }
+
+    public double perimeter() {
+	return 2.0 *Math.PI * radius_;
+    }
+
+    public String toString() {
+	return new String( "Circle:id=" + super.id_ + ":radius=" + radius_);
+    }
+
+    // test
+    public static void main( String [] args ) {
+	Circle c = new Circle( 1, 3.3 );
+	System.out.println(c + ":area=" +c.area() +":perimeter=" + c.perimeter() );
+    }
+
+}
---
+++ Examples/src/Shape.java
@@ -0,0 +1,26 @@
+public abstract class Shape {
+
+    protected long id_;
+
+    public Shape() {
+	id_=0;
+    }
+
+    public Shape( long id ) {
+	id_=id;
+    }
+
+    public Shape( Shape s ) {
+	id_=s.id_;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    // Test 
+    public static void main( String [] args ) {
+	//Shape s = new Shape();
+    }
+
+
+}
