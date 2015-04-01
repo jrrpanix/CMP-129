c75c91830a711b71b08451ad22f1cf3b275b944c
jrrpanix <jrr@panix.com>
2015-04-01 06:20:37 -0400

    rev

--- Examples/src/StringEntryView.java
+++ Examples/src/StringEntryView.java
@@ -1,8 +1,9 @@
-import javax.swing.*;
+// Course      : CMP-129
+// Instructor  : JReynolds
+// Title       : Example showing usage of ActionListener and KeyListener
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.*;
 
 public class StringEntryView extends JFrame{
