8ba39f233a93b9ac2576a7fa2841b4069f5ef996
John Reynolds <john@Johns-iMac-2.local>
2016-01-31 06:53:46 -0500

    add

---
+++ src/intro/Keyboard.java
@@ -0,0 +1,47 @@
+package lecture1;
+
+import java.util.Scanner;
+
+public class Keyboard {
+
+	public static String getLine( Scanner scan) {
+		return scan.nextLine();
+	}
+	
+	public static String [] getArgs(String line) {
+		return line.split(" ");
+	}
+	
+	public static String [] getFromKeyboard( Scanner scan) {
+		String line = scan.nextLine().trim();
+		String [] lineArgs=getArgs(line);
+		return lineArgs;
+	}
+	
+	public static Scanner createScanner() {
+		Scanner scan = new Scanner(System.in);
+		return scan;
+	}
+	
+	public static boolean isDone( String [] args) {
+		return args.length == 0 || args[0].length() == 0 || args[0].equals("end");
+	}
+	
+	public static void run() {
+		System.out.println("enter data");
+		Scanner scan = createScanner();
+		boolean more = true;
+		while( more) {
+			String [] data = getFromKeyboard(scan);
+			if ( isDone(data)) break;
+			Cmds.printArgs(data);
+		}
+		System.out.println("ended");
+	}
+	
+	
+	public static void main(String [] args) {
+		run();
+	}
+	
+}
