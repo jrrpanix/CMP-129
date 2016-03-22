274867a693974d615b57d85aa23d4f0ca5b7a311
unknown <jreynolds1@EH203-TEACHER-D.ccm.edu>
2016-03-21 20:28:40 -0400

    cmm

--- lectures/312/Example3.java
+++ lectures/312/Example3.java
@@ -3,9 +3,9 @@ package lecture7;
 import java.io.File;
 import java.util.Scanner;
 
-public class Example3 {
+public class Example3 { 
 	
-	public static void read( String fileName, int ix ) {
+	public static void read( String fileName, int ix ) throws BadCsvFileException {
 		try {
 			File f = new File(fileName);
 			Scanner scan = new Scanner(f);
@@ -23,7 +23,7 @@ public class Example3 {
 				}
 			}
 			catch( NumberFormatException e ) {
-				e.printStackTrace(System.err);
+				throw new BadCsvFileException("file" + fileName);
 			}
 			//catch( Exception e) {
 			//	System.out.println("catching generic exception " + e.getMessage());
@@ -37,14 +37,17 @@ public class Example3 {
 		}
 	}
 	
+	public static void callRead( String fname , int ix ) {
+		try{
+			read(fname,ix);
+		}catch (BadCsvFileException e) {
+			
+		}
+	}
+	
 	public static void main( String [] args ) {
 		String fname = "C://Users/jreynolds1/git/CMP-129/data/google_prices.csv";
-		read(fname,0);
-		read(fname,1);
-		read(fname,2);
-		read(fname,3);
-		read(fname,111);
-		read(fname,4);
+		callRead(fname,0);
 	}
 	
 	
